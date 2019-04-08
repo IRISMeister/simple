cc01becc0425a7a79cbdb2cd8ac0252ab4f63922
iwamoto <iwamoto@dummy>
2019-04-08 15:02:04 +0900

    beautify Dockefile

--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
+#Build
 # ./build.sh
+#Run
 # ./run.sh
-#
-#
-# Test
-# curl -H "Content-Type: application/json; charset=UTF-8" -H "Accept:application/json" "http://localhost:52773/csp/myapp/test" --user "user:pass"
+#Test
+# /test.sh
--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ cp prod/pwd.isc prod/etc/
 rm -f prod/etc/pwd.isc.done
 docker run --rm --name myiris -h myiris -p 51773:51773 -p 52773:52773 \
        -v /etc/localtime:/etc/localtime:ro \
-       -v $PWD/prod:/ISC dpmeister/irisdemo:simple \
+       -v $PWD/prod:/ISC \
+       dpmeister/irisdemo:simple \
        -p /ISC/etc/pwd.isc
