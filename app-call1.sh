1e827c4559b4d51992bf20b9b01a180023f85891
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2019-07-30 14:26:43 +0900

    some fix

---
+++ app-call1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+   ip=localhost
+else
+   ip=$1
+fi
+curl -H "Content-Type: application/json; charset=UTF-8" -H "Accept:application/json" "http://$ip:52773/csp/myapp/get" --user "SuperUser:sys"
--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cp prod/pwd.isc prod/etc/
 rm -f prod/etc/pwd.isc.done
-docker run --rm --name myiris -h myiris -p 51773:51773 -p 52773:52773 \
-       -v /etc/localtime:/etc/localtime:ro \
+docker run --init -d --rm --name myiris -h myiris -e TZ=Asia/Tokyo -p 51773:51773 -p 52773:52773 \
        -v $PWD/prod:/ISC \
        dpmeister/irisdemo:simple \
        -p /ISC/etc/pwd.isc
