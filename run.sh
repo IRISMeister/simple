b3682d28a66c565a1f138f8616d25c91f461213d
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-01-31 16:10:12 +0900

    simplified

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 docker-compose up -d
-# comment out below lines if you want to externalize database files.
+# comment out below lines if you want to externalize user database file(s).
 #docker-compose exec irissvc bash -c "\$ISC_PACKAGE_INSTALLDIR/dev/Cloud/ICM/waitISC.sh '' 30"
 #./externalize.sh
