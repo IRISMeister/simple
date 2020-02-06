67319578d24f56c7eb75d00109a6f8a877fd8856
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-02-06 15:10:12 +0900

    actions

--- unittest.sh
+++ unittest.sh
@@ -2,7 +2,9 @@
 docker-compose up -d
 docker-compose exec irissvc bash -c "\$ISC_PACKAGE_INSTALLDIR/dev/Cloud/ICM/waitISC.sh '' 10"
 docker-compose exec irissvc bash -c "iris session iris -U MYAPP < unittest.cos"
-printf '\nExit status:'$?'\n'
+teststatus=$?
+printf '\nExit status:'$teststatus'\n'
 
 docker-compose down -v
 # docker-compose push
+exit $teststatus
