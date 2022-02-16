3146411aafd7aaf51fc94a8dd665b6f5caf00515
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2022-02-16 13:27:40 +0900

    change owner

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+sudo rm -fR external-vol-db/*
+sudo rm -fR external-vol-mgr/*
+sudo chown 51773:51773 external-vol-db
+sudo chown 51773:51773 external-vol-mgr
 docker-compose up -d
--- unittest.sh
+++ unittest.sh
@@ -1,4 +1,8 @@
 #!/bin/bash 
+sudo rm -fR external-vol-db/*
+sudo rm -fR external-vol-mgr/*
+sudo chown 51773:51773 external-vol-db
+sudo chown 51773:51773 external-vol-mgr
 docker-compose up -d
 docker-compose exec -T iris bash -c "\$ISC_PACKAGE_INSTALLDIR/dev/Cloud/ICM/waitISC.sh '' 60"
 docker-compose exec -T iris bash -c "iris session iris -U MYAPP < /home/irisowner/cos/unittest.cos"
