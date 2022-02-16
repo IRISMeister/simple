773a0ada07ea794d4024dd758b6d9f1f8cee619c
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2022-02-16 12:44:27 +0900

    k8s

--- k8s/delete.sh
+++ k8s/delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-microk8s kubectl delete -f simple.yml
-microk8s kubectl delete pvc dbv-data-iris-0 dbv-mgr-iris-0
+microk8s kubectl delete -f simple.yml --wait
+microk8s kubectl delete pvc -l app=iris
