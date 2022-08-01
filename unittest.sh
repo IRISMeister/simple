a2536251b9dba58caaedf26648eed8c3a112fa0b
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2022-08-01 18:03:35 +0900

    add yml for unittest

--- unittest.sh
+++ unittest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
-docker-compose up -d
-docker-compose exec -T iris bash -c "\$ISC_PACKAGE_INSTALLDIR/dev/Cloud/ICM/waitISC.sh '' 60"
-docker-compose exec -T iris bash -c "iris session iris -U MYAPP < /home/irisowner/cos/unittest.cos"
+docker-compose -f docker-compose-unittest.yml up -d
+docker-compose -f docker-compose-unittest.yml exec -T iris bash -c "\$ISC_PACKAGE_INSTALLDIR/dev/Cloud/ICM/waitISC.sh '' 60"
+docker-compose -f docker-compose-unittest.yml exec -T iris bash -c "iris session iris -U MYAPP < /home/irisowner/cos/unittest.cos"
 teststatus=$?
 printf '\nExit status from unittests:'$teststatus'\n'
 
-docker-compose down -v
+docker-compose -f docker-compose-unittest.yml down -v
 # docker-compose push
 exit $teststatus
