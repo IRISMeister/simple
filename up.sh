6678daf798ea72bdd3f1735b2cee43715848cac0
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2024-05-01 16:47:59 +0900

    fix

--- up.sh
+++ up.sh
@@ -4,3 +4,5 @@ docker compose up -d
 docker compose exec -T iris bash -c "\$ISC_PACKAGE_INSTALLDIR/dev/Cloud/ICM/waitISC.sh '' 60"
 # Loading sample data (in order to load them into database outside of a container)
 docker compose exec iris iris session iris -U MYAPP "##class(MyApp.Utils).LoadCSV()"
+
+#docker compose exec iris iris session iris -U USER "##class(MyApps.Installer).ConfigCSP()"
