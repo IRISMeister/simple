77151a95786f7b0a6bbc300e35d49570ad3d9cbf
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2024-05-24 13:29:27 +0900

    test

--- unittest.sh
+++ unittest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 docker compose -f docker-compose-unittest.yml up -d
 docker compose -f docker-compose-unittest.yml exec -T iris bash -c "\$ISC_PACKAGE_INSTALLDIR/dev/Cloud/ICM/waitISC.sh '' 60"
+docker compose ps
 docker compose -f docker-compose-unittest.yml exec -T iris bash -c "iris session iris -U MYAPP < /home/irisowner/cos/unittest.cos"
 teststatus=$?
 printf '\nExit status from unittests:'$teststatus'\n'
